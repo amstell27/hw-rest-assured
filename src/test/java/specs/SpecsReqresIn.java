b0420850a735f649cd85f7a2e87315d784e90ba8
amstell27 <sergei_i27@mail.ru>
2022-06-07 15:35:16 +0700

    add allure template

--- src/test/java/specs/SpecsReqresIn.java
+++ src/test/java/specs/SpecsReqresIn.java
@@ -4,12 +4,14 @@ import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
+import static helpers.CustomAllureListener.withCustomTemplates;
 import static io.restassured.RestAssured.with;
 
 public class SpecsReqresIn {
 
     public static RequestSpecification request = with()
-            .baseUri("https://reqres.in/")
+            .filter(withCustomTemplates())
+            .baseUri("https://reqres.in/api/")
             .log().body();
 
     public static ResponseSpecification responseSpec = new ResponseSpecBuilder()

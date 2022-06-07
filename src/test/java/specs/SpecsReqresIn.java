3e415f8d3c2410e92210a2d9d8ab4f074cdaf556
amstell27 <sergei_i27@mail.ru>
2022-06-07 12:02:07 +0700

    Add api-tests for reqresin

---
+++ src/test/java/specs/SpecsReqresIn.java
@@ -0,0 +1,18 @@
+package specs;
+
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+import static io.restassured.RestAssured.with;
+
+public class SpecsReqresIn {
+
+    public static RequestSpecification request = with()
+            .baseUri("https://reqres.in/")
+            .log().body();
+
+    public static ResponseSpecification responseSpec = new ResponseSpecBuilder()
+            .expectStatusCode(200)
+            .build();
+}
